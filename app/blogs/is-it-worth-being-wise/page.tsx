d9efdd7fc6262400fa08cfd4a192c8809ba5c331
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-06-02 19:00:40 -0400

    hi

---
+++ app/blogs/is-it-worth-being-wise/page.tsx
@@ -0,0 +1,17 @@
+import link from 'next/link';
+
+const blog1 = () => {
+
+    return (
+        <div className="flex min-h-screen flex-col bg-white">
+        <main className="max-w-4xl mx-auto w-full mt-10">
+            <h1 className="text-2xl font-bold text-gray-800">Is it Worth Being Wise?</h1>
+            <p className="text-gray-600 dark:text-gray-600">
+           I need to write this blog post sorry didn't have time to write it yet.
+            </p>
+        </main>
+        </div>
+    );
+    }
+
+
