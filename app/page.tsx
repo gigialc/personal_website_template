8d44c16011496d2389771e755bac40e0dcbfe8cd
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-06-02 18:58:01 -0400

    hi

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="py-4 px-3">
           <ul className="space-y-2">
             <li>
-              <a href="/blogs" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white">
+              <a href="/blogs" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-black">
                 Blogs
               </a>
             </li>
