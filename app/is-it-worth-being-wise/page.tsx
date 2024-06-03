9a95826b84448439080667a270c3dd8d88210ffc
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-06-03 09:51:32 -0400

    hi

--- app/is-it-worth-being-wise/page.tsx
+++ app/is-it-worth-being-wise/page.tsx
@@ -6,7 +6,7 @@ const blog1 = () => {
         <div className="flex min-h-screen flex-col bg-white">
         <main className="max-w-4xl mx-auto w-full mt-10">
             <h1 className="text-2xl font-bold text-gray-800 my-4 pl-5">Is it Worth Being Wise?</h1>
-            <p className="text-gray-600 dark:text-gray-600 my-4 pl-5">
+            <p className="text-gray-600 dark:text-gray-600 my-4 pl-5 mr-5">
            I need to write this blog post sorry didn't have time to write it yet.
             </p>
         </main>
