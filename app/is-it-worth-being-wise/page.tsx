5cd662f51492e6bbae2c5314ad0cc59daf3f7b0e
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-06-02 20:06:10 -0400

    lol

--- app/is-it-worth-being-wise/page.tsx
+++ app/is-it-worth-being-wise/page.tsx
@@ -1,18 +1,17 @@
 import link from 'next/link';
 
 const blog1 = () => {
-
     return (
-        <div className="flex min-h-screen flex-col bg-white">
-        <main className="max-w-4xl mx-auto w-full mt-10">
-            <h1 className="text-2xl font-bold text-gray-800 my-4">Is it Worth Being Wise?</h1>
-            <p className="text-gray-600 dark:text-gray-600 my-4">
-           I need to write this blog post sorry didn't have time to write it yet.
-            </p>
+      <div className="flex min-h-screen flex-col bg-white">
+        <main className="max-w-4xl mx-auto w-full mt-10 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-2xl font-bold text-gray-800 my-4">Is it Worth Being Wise?</h1>
+          <p className="text-gray-600 dark:text-gray-600 my-4">
+            I need to write this blog post sorry didn't have time to write it yet.
+          </p>
         </main>
-        </div>
+      </div>
     );
-    }
-
-export default blog1;
+  }
+  
+  export default blog1;
 
