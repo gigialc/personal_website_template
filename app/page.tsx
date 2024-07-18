164943b5fad6250643d0a244fe6ebc69d7250ea3
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-07-18 01:23:35 -0400

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,90 +1,94 @@
 import Image from 'next/image';
-
+import { Mail, Linkedin, Twitter, Github } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <main className="min-h-screen bg-white dark:bg-white p-4 sm:p-10">
-        <aside className="w-48" aria-label="Sidebar"> {/* Reduced width to 48 from 64 */}
-          <div className="py-4 px-3">
-            <ul className="space-y-2">
-              <li>
-                <a href="/blogs" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-black">
-                  Blogs
-                </a>
-              </li>
-            </ul>
-          </div>
-        </aside>
+    <div className="flex bg-white flex-col">
+      <main className="min-h-screen p-4 sm:p-10">
         <div className="max-w-4xl mx-auto w-full">
-          <header className="mb-10">
-            <h1 className="text-4xl font-bold text-gray-800 dark:text-black">Gigi Alcaraz 👩🏼‍💻</h1>
+
+          <aside className="w-48" aria-label="Sidebar">
+            <div className="py-4 px-3">
+              <ul className="space-y-2">
+                <li>
+                  <a href="/blogs" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-rose-200 transition-colors">
+                    Thoughts 💭
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </aside>
+
+          <header className="mb-10 text-center">
+            <h1 className="text-3xl font-bold text-gray-800 mb-2">Gigi Alcaraz</h1>
           </header>
 
-          <div className="flex justify-right">
+          <div className="flex justify-center mb-10">
             <Image
               src="/georgina.jpg"
-              alt="Picture of the author"
+              alt="Gigi Alcaraz"
               width={300}
               height={300}
-              className="rounded-full"
+              className="rounded-full shadow-lg"
             />
           </div>
-          <section className="mb-6 mt-10">
-            <p className="text-gray-600 dark:text-gray-600">
-              On a mission to increase women's health literacy ❤️ Blockchain enthusiast 🚀
+
+          <section className="mb-12 text-center">
+            <p className="text-base text-gray-700 leading-relaxed">
+              building in ai and blockchain right now  👩‍💻💌🪩🩷 
+              ...also figuring it out
             </p>
+            <br />
+             {/* Social icons at the top */}
+          <div className="flex justify-center space-x-4 mb-8">
+            <a href="mailto:galcaraz@bu.edu" className="text-gray-600 hover:text-teal-600 transition-colors">
+              <Mail size={24} />
+            </a>
+            <a href="https://www.linkedin.com/in/georgina-alcaraz/" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-teal-600 transition-colors">
+              <Linkedin size={24} />
+            </a>
+            <a href="https://twitter.com/gigiialc" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-teal-600 transition-colors">
+              <Twitter size={24} />
+            </a>
+            <a href="https://github.com/gigialc" target="_blank" rel="noopener noreferrer" className="text-gray-600 hover:text-teal-600 transition-colors">
+              <Github size={24} />
+            </a>
+          </div>
           </section>
 
-          <section className="mb-6">
-            <h2 className="text-2xl text-gray-900 dark:text-black mb-3">Projects and Apps</h2>
-            <ul className="list-disc list-inside text-gray-900 dark:text-gray-600">
-              <br/>
-              <li className="list-disc">
-                I'm running the <a href="https://womenxinnovators.wixsite.com/letsinnovate" className="text-pink-500 hover:text-pink-700" target="_blank" rel="noopener noreferrer">Womenx Innovators</a> community for female founders!
-              </li>
-              <br/>
-              <li className="list-disc">
-                I'm building <a href="https://tryavocadohealth.com/" className="text-pink-500 hover:text-pink-700" target="_blank" rel="noopener noreferrer">Avocado Health 🥑</a>
-              </li>
-              <br/>
-             <li className="list-disc">
-                Building <a href="https://verve-health.vercel.app/" className="text-pink-500 hover:text-pink-700" target="_blank" rel="noopener noreferrer">verve</a>, an app for women to find affordable healthcare solutions personalized to their needs.
-              </li> 
-            </ul>
+          
+
+          <section className="mb-12">
+            {/* <h2 className="text-2xl text-black mb-6 text-center">Projects</h2> */}
+            <div className="space-y-6">
+              <div className="bg-white p-6 hover:shadow-xl transition-shadow">
+                <h3 className="text-xl font-semibold mb-2">Womenx Innovators</h3>
+                <p className="text-gray-600 mb-4">Fostering a community of visionary female founders, driving innovation and change.</p>
+                <a href="https://womenxinnovators.wixsite.com/letsinnovate" className="text-teal-600 hover:text-teal-800 font-medium" target="_blank" rel="noopener noreferrer">Explore the community →</a>
+              </div>
+              <div className="bg-white p-6 hover:shadow-xl transition-shadow">
+                <h3 className="text-xl font-semibold mb-2">Avocado Health 🥑</h3>
+                <p className="text-gray-600 mb-4">AI copilot for safe health content generation.</p>
+                <a href="https://tryavocadohealth.com/" className="text-teal-600 hover:text-teal-800 font-medium" target="_blank" rel="noopener noreferrer">Discover Avocado Health →</a>
+              </div>
+              <div className="bg-white p-6 hover:shadow-xl transition-shadow">
+                <h3 className="text-xl font-semibold mb-2">Verve 💜</h3>
+                <p className="text-gray-600 mb-4">Empowering women with tailored, affordable healthcare solutions.</p>
+                <a href="https://verve-health.vercel.app/" className="text-teal-600 hover:text-teal-800 font-medium" target="_blank" rel="noopener noreferrer">Experience Verve →</a>
+              </div>
+            </div>
           </section>
 
-          <section className="mb-6">
-            <h2 className="text-2xl text-gray-900 dark:text-black mb-3">Experience</h2>
-            <ul className="list-disc list-inside text-gray-900 dark:text-gray-600">
-            <li>
-                I worked at <a href="https://anetabtc.io/" className="text-pink-500 hover:text-pink-700" target="_blank" rel="noopener noreferrer">AnetaBTC</a> building the community and implementing tokenomics and decentralized finance! (startup)
-              </li>
-              <br/>
-              <li>
-                Technology risk management intern @<a href="https://www.statestreet.com/us/en/asset-manager" className="text-pink-500 hover:text-pink-700" target="_blank" rel="noopener noreferrer">State Street.</a> Digital assets and quantum encryption in banking.
-              </li>
-            </ul>
+          <section className="mb-12">
+            <h2 className="text-3xl text-gray-800 mb-6 text-center">More about me...</h2>
+            <div className="bg-white p-6">
+              <ul className="list-disc list-inside text-gray-700 space-y-3">
+                <li>I have gone skydiving twice in my life and was a competitive alpine skier at one point.</li>
+                <li>I learned how to code in high school because I wanted to channel my creativity into building for others.</li>
+                <li>I'm originally from Spain 🇪🇸</li>
+              </ul>
+            </div>
           </section>
-        <br/>
-        
-          <section className="mb-6">
-            <h2 className="text-2xl text-gray-900 dark:text-black mb-3">More about me...</h2>
-            <ul className="list-disc list-inside text-gray-900 dark:text-gray-600">
-              <li className="list-disc">I love sports, I have gone skydiving twice in my life, I also love skiing and horseback riding</li>
-              {/* <li>I'm from Barcelona, Spain ☀️ and I'm currently an undergraduate student at BU majoring in Business Analytics</li> */}
-              <br/>
-              <li className="list-disc">I learned how to code in high school because I wanted to be able to create things</li>
-            </ul>
-          </section>
-
-          <footer className="mb-10">
-            <h2 className="text-2xl text-gray-900 dark:text-black mb-3">Contact 💌</h2>
-            <p className="text-gray-600 dark:text-gray-600">Email: galcaraz@bu.edu </p>
-            <p className="text-gray-600 dark:text-gray-600">LinkedIn: <a href="https://www.linkedin.com/in/georgina-alcaraz/" className="text-blue-500 hover:text-blue-700">LinkedIn</a></p>
-            <p className="text-gray-600 dark:text-gray-600">Twitter: <a href="https://twitter.com/gigiialc" className="text-blue-500 hover:text-blue-700">Twitter</a></p>
-            <p className="text-gray-600 dark:text-gray-600">Github: <a href="https://github.com/gigialc" className="text-blue-500 hover:text-blue-700">Github</a></p>
-          </footer>
         </div>
       </main>
     </div>
